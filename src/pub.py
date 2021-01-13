0b4d8ba9b10989889aa7652b4300d9c3f9deeb86
Scott Coupar <scottcoupar@Scotts-MacBook-Pro.local>
2021-01-13 10:35:14 +0000

    first commit

---
+++ src/pub.py
@@ -0,0 +1,5 @@
+class Pub:
+    
+    def __init__(self, name, total_cash):
+        self.name = name
+        self.total_cash = total_cash
---
+++ tests/pub_test.py
@@ -0,0 +1,11 @@
+import unittest
+from src.pub import Pub
+
+class TestPub(unittest.TestCase): #inherits TestCase from unittest
+    def setUp(self):
+        self.pub = Pub("The Prancing Pony", 100.00)
+
+
+    def test_pub_has_name(self):
+        self.assertEqual("The Prancing Pony", self.pub.name)
+
